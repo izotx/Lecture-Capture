3fc30b14c4dcf2f25b40155a6a484325b43177c2
Janusz Chudzynski <jmc72@node-143088091058.u.uwf.edu>
2012-04-27 07:32:29 -0500

    Initial Commit

---
+++ Recorder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Recorder
+//
+//  Created by Janusz Chudzynski on 4/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
